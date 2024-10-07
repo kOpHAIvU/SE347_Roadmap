36d79ffb9f34d4b90889054f50fa609a15d54a3f
KoPhaiVu <hoangle293vu@gmail.com>
2024-10-07 09:47:44 +0700

    Build(Frontend): custom node

--- client/src/page/OwnRoadmap/index.js
+++ client/src/page/OwnRoadmap/index.js
@@ -2,23 +2,20 @@ import { useEffect, useRef, useState } from 'react';
 import styles from './OwnRoadmap.module.scss';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare as penSolid, faSquareCheck } from '@fortawesome/free-solid-svg-icons';
+import { faSquare, faSquarePlus, faTrashCan, faPenToSquare as penRegular, faCircle } from '@fortawesome/free-regular-svg-icons';
 
 const cx = classNames.bind(styles);
 
 function OwnRoadmap({ roadmapName = 'Name not given',
     content = 'GitHub là một hệ thống quản lý dự án và phiên bản code, hoạt động giống như một mạng xã hội cho lập trình viên. Các lập trình viên có thể clone lại mã nguồn từ một repository và Github chính là một dịch vụ máy chủ repository công cộng, mỗi người có thể tạo tài khoản trên đó để tạo ra các kho chứa của riêng mình để có thể làm việc. GitHub có 2 phiên bản: miễn phí và trả phí. Với phiên bản có phí thường được các doanh nghiệp sử dụng để tăng khả năng quản lý team cũng như phân quyền bảo mật dự án. Còn lại thì phần lớn chúng ta đều sử dụng Github với tài khoản miễn phí để lưu trữ source code.'
-    , }) {
+    , node = 'Install the environment' }) {
     console.log('Rendering OwnRoadmap');
+    const [roadName, setRoadName] = useState(roadmapName);
     const [contentExpanded, setIsContentExpanded] = useState(false);
     const [contentText, setContentText] = useState(content);
     const [isEditing, setIsEditing] = useState(false);
     const textareaRef = useRef(null);
-
-
-    const toggleEditMode = () => {
-        setIsEditing(!isEditing);
-    };
 
     const adjustTextareaHeight = () => {
         if (textareaRef.current) {
@@ -35,9 +32,26 @@ function OwnRoadmap({ roadmapName = 'Name not given',
         }
     }, [isEditing, contentText]);
 
+    const [tickArray, setTickArray] = useState(Array(10).fill(false));
+
+    const toggleTickArray = (index) => {
+        setTickArray((prevArray) =>
+            prevArray.map((value, i) => (i === index ? !value : value))
+        );
+    };
+
+    const [rewriteNode, setRewriteNode] = useState(node);
+    const [isRewriteNode, setIsRewriteNode] = useState(false);
+
+
     return (
         <div className={cx('wrapper')}>
-            <h1 className={cx('page-title')}>{roadmapName}</h1>
+            <input
+                className={cx('page-title')}
+                value={roadName}
+                onChange={(e) => setRoadName(e.target.value)}
+            />
+
             <div className={cx('content-section')}>
                 {isEditing ? (
                     <textarea
@@ -45,7 +59,7 @@ function OwnRoadmap({ roadmapName = 'Name not given',
                         type="text"
                         value={contentText}
                         onChange={(e) => setContentText(e.target.value)}
-                        onBlur={toggleEditMode} // Khi mất focus, quay lại chế độ xem
+                        onBlur={() => setIsEditing(!isEditing)} // Khi mất focus, quay lại chế độ xem
                         className={cx('content-input')}
                         autoFocus // Tự động focus vào input khi chuyển sang chế độ chỉnh sửa
                     />
@@ -59,9 +73,56 @@ function OwnRoadmap({ roadmapName = 'Name not given',
                 )}
                 <FontAwesomeIcon
                     className={cx('rewrite-content-btn')}
-                    icon={faPenToSquare}
-                    onClick={toggleEditMode}
+                    icon={penSolid}
+                    onClick={() => setIsEditing(!isEditing)}
                 />
+            </div>
+
+            <div className={cx('roadmap-section')}>
+                <div className={cx('level-one')}>
+                    <div className={cx('show-section')}>
+                        {tickArray[1] ? (
+                            <FontAwesomeIcon
+                                onClick={() => toggleTickArray(1)}
+                                icon={faSquareCheck}
+                                className={cx('ticked')} />
+                        ) : (
+                            <div
+                                onClick={() => toggleTickArray(1)}
+                                className={cx('tick')} />
+                        )}
+                        {isRewriteNode ? (
+                            <input
+                                className={cx('level-one-content-edit')}
+                                type="text"
+                                value={rewriteNode}
+                                onChange={(e) => setRewriteNode(e.target.value)}
+                                onBlur={() => setIsRewriteNode(!isRewriteNode)}
+                                autoFocus />
+                        ) : (
+                            <h1 className={cx('level-one-content')}>{rewriteNode}</h1>
+                        )}
+
+                        {isRewriteNode ? (
+                            <FontAwesomeIcon
+                                icon={faTrashCan}
+                                className={cx('remove-node')} />
+                        ) : (
+                            <FontAwesomeIcon
+                                onClick={() => setIsRewriteNode(!isRewriteNode)}
+                                icon={penRegular}
+                                className={cx('rewrite-node')}
+                            />
+                        )}
+
+                    </div>
+
+                    <div className={cx('hidden-section')}>
+                        <FontAwesomeIcon className={cx('add-node')} icon={faSquarePlus} />
+                        <FontAwesomeIcon className={cx('add-node')} icon={faSquare} />
+                        <FontAwesomeIcon className={cx('add-node')} icon={faCircle} />
+                    </div>
+                </div>
             </div>
         </div>
     );
