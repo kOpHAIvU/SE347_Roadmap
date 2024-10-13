cef670a564939614460dde369d714ed00dd627a7
Pttvinh253 <[200~22521680@gm.uit.edu.vn>
2024-10-13 18:03:58 +0700

    Build(Frontend):Delete SignupLayout and LoginLayout

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -1,4 +1,3 @@
 export { default as DefaultLayout } from './DefaultLayout';
 export { default as MainScreenLayout } from './MainScreenLayout';
-export { default as LoginLayout } from './LoginLayout';
-export { default as SignupLayout } from './SignupLayout';
+
