b6dbe3945e39c9886128febb3f15f3ac5bf5978b
Pttvinh253 <[200~22521680@gm.uit.edu.vn>
2024-10-19 03:46:00 +0700

    Build(Frontend): hover avatar HeaderLogged

--- client/src/components/Layout/components/HeaderLogged/index.js
+++ client/src/components/Layout/components/HeaderLogged/index.js
@@ -1,12 +1,27 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './HeaderLogged.module.scss';
 import classNames from 'classnames/bind';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faCircleQuestion, faGear, faPlus, faQuestion, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import Search from '../Search/index.js';
 import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
+import MenuAvatar from '../MenuAvatar/index.js';
 
 const cx = classNames.bind(styles);
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faGear}/>,
+        title: 'Settings',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion}/>,
+        title: 'More about Vertex',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faRightFromBracket}/>,
+        title: 'Log out',
+    }
+];
 
 function HeaderLogged({ collapsed, setCollapsed }) {
     return <div className={cx('wrapper', { extend: collapsed })}>
@@ -25,7 +40,10 @@ function HeaderLogged({ collapsed, setCollapsed }) {
                     <FontAwesomeIcon className={cx('plus-icon')} icon={faPlus} />
                     <h1 className={cx('create-text')}>Create your own map</h1>
                 </button>
-                <img className={cx('avatar')} src="https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg" alt="Avatar" />
+                <MenuAvatar items={MENU_ITEMS}>
+                    <img className={cx('avatar')} src="https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg" alt="Avatar" />
+                </MenuAvatar>
+                
             </div>
         </div>
 
