42ed9ca616d24d43c8d398978183e6c6f4557243
KoPhaiVu <hoangle293vu@gmail.com>
2024-10-21 23:34:02 +0700

    Build(Frontend): Make automatic Search item and Roadmap item

--- client/src/components/Layout/components/RoadmapItem/index.js
+++ client/src/components/Layout/components/RoadmapItem/index.js
@@ -7,31 +7,29 @@ import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
-function RoadmapItem() {
-    const [loveState, setLoveState] = useState(false);
-
+function RoadmapItem({ key, children, onLoveChange }) {
     return (
         <div className={cx('wrapper')}>
             <div className={cx('container')}>
                 <img className={cx('roadmap-pic')}
-                    src="https://i.ebayimg.com/images/g/XI0AAOSw~HJker7R/s-l1600.webp"
+                    src={children.avatar}
                     alt="Roadmap picture" />
-                <h1 className={cx('title')}>Github</h1>
-                <h2 className={cx('content')}>GitHub là một dịch vụ cung cấp kho lưu trữ mã nguồn Git dựa trên nền web cho các dự án ph...</h2>
+                <h1 className={cx('title')}>{children.title}</h1>
+                <h2 className={cx('content')}>{children.content}</h2>
                 <div className={cx('below')}>
                     <button className={cx('clone-btn')}>
                         <FontAwesomeIcon className={cx('clone-icon')} icon={faCircleDown} />
                         <span className={cx('clone-title')}>Clone</span>
                     </button>
-                    <FontAwesomeIcon   
+                    <FontAwesomeIcon
                         onClick={(e) => {
                             e.stopPropagation(); // Ngăn sự kiện lan truyền lên container
-                            setLoveState(!loveState)
-                        }} 
-                        icon={loveState ? faSolidHeart : faHeart} 
+                            onLoveChange();  // Gọi hàm cập nhật trạng thái
+                        }}
+                        icon={children.loved ? faSolidHeart : faHeart}
                         className={cx('love')} />
                     <div className={cx('clone-num')}>
-                        <span className={cx('num')}>10,7k clones</span>
+                        <span className={cx('num')}>{children.clones} clones</span>
                         <FontAwesomeIcon className={cx('bolt-icon')} icon={faBolt} />
                     </div>
                 </div>
