9771c9496db25aa750b19f65797f4961f16210f8
Pttvinh253 <[200~22521680@gm.uit.edu.vn>
2024-10-25 19:29:14 +0700

    Build(Frontend): UI SettingTimeline

---
+++ client/src/page/Settings/Users/index.js
@@ -0,0 +1,64 @@
+const users = [
+    { 
+      id: 1, 
+      name: 'Baby Shark Do Doo', 
+      email: 'babydoo@lovely.sizuka', 
+      role: 'Administrator', 
+      avatar: 'https://i.pinimg.com/736x/e6/9d/09/e69d09cbbc827e4e117ce570f633120c.jpg'
+    },
+    { 
+      id: 2, 
+      name: 'Baby Do Doo', 
+      email: 'babydoo@lovely.sizuka', 
+      role: 'Editor', 
+      avatar: 'https://i.pinimg.com/736x/e6/9d/09/e69d09cbbc827e4e117ce570f633120c.jpg'
+    },
+    { 
+      id: 3, 
+      name: 'Shark Do Doo', 
+      email: 'babydoo@lovely.sizuka', 
+      role: 'Reviewer', 
+      avatar: 'https://i.pinimg.com/736x/e6/9d/09/e69d09cbbc827e4e117ce570f633120c.jpg'
+    },
+    { 
+      id: 4, 
+      name: 'Hai Dang Do', 
+      email: 'babydoo@lovely.sizuka', 
+      role: 'Reviewer', 
+      avatar: 'https://i.pinimg.com/736x/e6/9d/09/e69d09cbbc827e4e117ce570f633120c.jpg'
+    },
+    { 
+      id: 5, 
+      name: 'LOVE YOU CHUCAMO', 
+      email: 'babydoo@lovely.sizuka', 
+      role: 'Editor', 
+      avatar: 'https://i.pinimg.com/736x/e6/9d/09/e69d09cbbc827e4e117ce570f633120c.jpg'
+    },
+  ];
+  
+  const invites = [
+    // { id: 1, name: 'Doo Do Doo', email: 'babydoo@lovely.sizuka', status: 'Pending' },
+    // { id: 2, name: 'Dang Doo', email: 'babydoo@lovely.sizuka', status: 'Pending' },
+    // { id: 3, name: 'Đỗ Hải Đăng', email: 'babydoo@lovely.sizuka', status: 'Pending' },
+    { 
+      id: 1, 
+      name: 'Baby Shark Do Doo', 
+      email: 'babydoo@lovely.sizuka', 
+      avatar: 'https://i.pinimg.com/736x/e6/9d/09/e69d09cbbc827e4e117ce570f633120c.jpg'
+    },
+    { 
+      id: 2, 
+      name: 'Baby Do Doo', 
+      email: 'babydoo@lovely.sizuka', 
+      avatar: 'https://i.pinimg.com/736x/e6/9d/09/e69d09cbbc827e4e117ce570f633120c.jpg'
+    },
+    { 
+      id: 3, 
+      name: 'Shark Do Doo', 
+      email: 'babydoo@lovely.sizuka', 
+      avatar: 'https://i.pinimg.com/736x/e6/9d/09/e69d09cbbc827e4e117ce570f633120c.jpg'
+    }
+  ];
+  
+export { users, invites };  
+export default { users, invites }; 
