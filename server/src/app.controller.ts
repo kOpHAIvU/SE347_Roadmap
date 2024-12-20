a69d94011e09b0438ab3a7107077b49903afd6f7
ITechIOI <22520783@gm.uit.edu.vn>
2024-12-20 11:37:33 +0700

    feat(API): Update payment by zalopay service

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
 import { JwtAuthGuard } from './modules/auth/common/jwt-guard';
 import { Roles } from './modules/role/common/role.decorator';
 import { RoleGuard } from './modules/role/common/role.guard';
+import { MomoService } from './modules/payment/strategy/momo.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly momoService: MomoService
+  ) {}
 
   @Get()
   getHello(): string {
